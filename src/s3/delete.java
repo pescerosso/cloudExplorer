6e8f8d064e62fc7c2879efac95682fa252a7d1af
ptribble <ptribble@shibuya.geminimobile.com>
2013-11-22 15:49:36 -0800

    Fixes

--- src/s3/delete.java
+++ src/s3/delete.java
@@ -3,13 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package s3;
 
-/**
- *
- * @author ptribble
- */
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteBucketRequest;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class delete {
-    
+
+    void deleteBucket(String access_key, String secret_key, String bucket, String endpoint) {
+        AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
+
+        AmazonS3 s3Client = new AmazonS3Client(credentials);
+        s3Client.setEndpoint(endpoint);
+
+        try {
+            s3Client.deleteBucket(new DeleteBucketRequest(bucket));
+        } catch (Exception Delete) {
+            System.out.print("\n\nAn error has occured in DeleteBucket.");
+            System.out.println("\n\nError Message:    " + Delete.getMessage());
+            System.out.print("\nDebug: Access key: " + access_key);
+            System.out.print("\nDebug: Secret key: " + secret_key);
+        }
+    }
+
+    void deleteFile(String what, String access_key, String secret_key, String bucket, String endpoint) {
+        AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
+        File file = new File(what);
+        AmazonS3 s3Client = new AmazonS3Client(credentials);
+        s3Client.setEndpoint(endpoint);
+
+        try {
+            s3Client.deleteObject(new DeleteObjectRequest(bucket, what));
+        } catch (Exception Delete) {
+            System.out.print("\n\nAn error has occured in DeleteFile.");
+            System.out.println("\n\nError Message:    " + Delete.getMessage());
+            System.out.print("\nDebug: Access key: " + access_key);
+            System.out.print("\nDebug: Secret key: " + secret_key);
+            System.out.print("\nDebug: key:" + what);
+        }
+    }
 }
