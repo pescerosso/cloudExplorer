169058eec7702c03458388dcacef3dfb32430ed0
ptribble <ptribble@shibuya.geminimobile.com>
2013-11-22 15:25:26 -0800

    Fixes

--- src/s3/get.java
+++ src/s3/get.java
@@ -20,7 +20,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
+foo
 public class get {
 
     void writeFile(InputStream is, String where) {
