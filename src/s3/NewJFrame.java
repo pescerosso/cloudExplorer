452f48a3b03e075409d7b0a49408eb8605b572d9
Phillip Tribble <philliptribble@Phillips-MacBook-Air.local>
2013-11-24 20:39:55 -0800

    Fixes

--- src/s3/NewJFrame.java
+++ src/s3/NewJFrame.java
@@ -4,8 +4,6 @@
  */
 package s3;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.BufferedReader;
