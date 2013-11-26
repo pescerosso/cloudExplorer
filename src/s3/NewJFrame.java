2e24abf545a0845bdfe81c49555627cfae439a47
Phillip Tribble <philliptribble@Phillips-MacBook-Air.local>
2013-11-25 18:57:20 -0800

    Fixes

--- src/s3/NewJFrame.java
+++ src/s3/NewJFrame.java
@@ -15,9 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class NewJFrame extends javax.swing.JFrame implements ItemListener {
 
