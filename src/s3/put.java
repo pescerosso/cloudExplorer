89dfddc093a1e06a07ae0eb09e05252afddd48aa
philliptribble <philliptribble@192.168.1.91>
2013-11-27 14:23:18 -0800

    Fix for content-type

--- src/s3/put.java
+++ src/s3/put.java
@@ -4,7 +4,9 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import java.io.File;
 
 public class put {
@@ -13,14 +15,18 @@ public class put {
 
     String put(String what, String access_key, String secret_key, String bucket, String endpoint, String ObjectKey) {
         File file = new File(what);
-
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType("text/html");
+       
         AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
-
+      
         AmazonS3 s3Client = new AmazonS3Client(credentials);
         s3Client.setEndpoint(endpoint);
-
+        PutObjectRequest putRequest = new PutObjectRequest(bucket, ObjectKey, file);
+        putRequest.setMetadata(objectMetadata);
+        
         try {
-            s3Client.putObject(new PutObjectRequest(bucket, ObjectKey, file));
+           PutObjectResult response = s3Client.putObject(putRequest);     
             message = ("Put object: " + what);
         } catch (Exception put) {
             System.out.print("\n\nAn error has occured with PUT.");
