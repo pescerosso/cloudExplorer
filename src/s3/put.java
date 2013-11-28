bdc450f4ac04db057b198cf0da3287447e887da9
Phillip Tribble <philliptribble@Phillips-MacBook-Air.local>
2013-11-27 22:28:45 -0800

    Fixes

--- src/s3/put.java
+++ src/s3/put.java
@@ -7,23 +7,31 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import javax.activation.MimetypesFileTypeMap;
 
 public class put {
 
     String message = null;
 
     String put(String what, String access_key, String secret_key, String bucket, String endpoint, String ObjectKey) {
-        File file = new File(what);
+        File file = new File(what);   
+        MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        String mimeType = mimeTypesMap.getContentType(file);
+        mimeType = mimeTypesMap.getContentType(file);
         ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType("text/html");
-       
+        objectMetadata.setContentType(mimeType);     
         AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
       
         AmazonS3 s3Client = new AmazonS3Client(credentials);
         s3Client.setEndpoint(endpoint);
         PutObjectRequest putRequest = new PutObjectRequest(bucket, ObjectKey, file);
-        putRequest.setMetadata(objectMetadata);
+         putRequest.setMetadata(objectMetadata);
         
         try {
            PutObjectResult response = s3Client.putObject(putRequest);     
