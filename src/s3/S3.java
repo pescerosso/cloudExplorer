b939b753ad17307074bcdb3f4cff27ffb163e630
Phillip Tribble <philliptribble@Phillips-MacBook-Air.local>
2013-12-04 20:03:07 -0800

    Put NewJFrame in TRY to try and prevent NPE errors

--- src/s3/S3.java
+++ src/s3/S3.java
@@ -1,13 +1,14 @@
-
 package s3;
-
 
 public class S3 {
 
     public static void main(String[] args) {
-       
-        NewJFrame gui = new NewJFrame();
-        gui.main(args);
 
+        try {
+            NewJFrame gui = new NewJFrame();
+            gui.main(args);
+        } catch (Exception S3) {
+
+        }
     }
 }
