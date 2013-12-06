afaf4f74e930ea4f0818425947bbd1599a8f7c37
ptribble <ptribble@shibuya.geminimobile.com>
2013-12-06 10:04:33 -0800

    Fix

--- src/s3/NewJFrame.java
+++ src/s3/NewJFrame.java
@@ -259,7 +259,7 @@ public class NewJFrame extends javax.swing.JFrame implements ItemListener {
 
         jPanel7.setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel13.setText("Version: 1.1. ");
+        jLabel13.setText("Version: Devel");
         jLabel13.setToolTipText("");
 
         jLabel14.setText("Please submit bugs via github: https://github.com/rusher81572/s3 ");
