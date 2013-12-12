8333ad73e50d6c6f2e4d467e49ed0653f41ce417
Phillip Tribble <philliptribble@Phillips-MacBook-Air.local>
2013-12-12 09:02:12 -0800

    Adding daemon class and added args to S3.java

--- src/s3/S3.java
+++ src/s3/S3.java
@@ -1,27 +1,39 @@
 package s3;
 
 public class S3 {
-    
     /*
- * Cloudian Explorer Copyright (C) December 20, 2013 By Phillip Tribble. This program
- * is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation;
- * either version 3 of the License, or (at your option) any later version. This
- * program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Softw4are Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
-**/
-    
+     * Cloudian Explorer Copyright (C) December 20, 2013 By Phillip Tribble. This program
+     * is free software; you can redistribute it and/or modify it under the terms of
+     * the GNU General Public License as published by the Free Software Foundation;
+     * either version 3 of the License, or (at your option) any later version. This
+     * program is distributed in the hope that it will be useful, but WITHOUT ANY
+     * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+     * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, write to the Free Softw4are Foundation, Inc., 59 Temple
+     * Place, Suite 330, Boston, MA 02111-1307 USA
+     **/
+
+    private static StringBuffer argss = new StringBuffer();
+
+    public static void setArgs(String data) {
+        argss.append(data);
+    }
+
+    public static String getArgs() {
+        return argss.toString();
+    }
+
     public static void main(String[] args) {
+        if (getArgs().contains("daemon")) {
 
-        try {
-            NewJFrame gui = new NewJFrame();
-            gui.main(args);
-        } catch (Exception S3) {
+        } else {
+            try {
+                NewJFrame gui = new NewJFrame();
+                gui.main(args);
+            } catch (Exception S3) {
 
+            }
         }
     }
 }
