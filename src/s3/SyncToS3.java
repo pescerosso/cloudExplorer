c9b1c4fd170c74deda88dd8c9f0c6319bf80bfc4
Phillip Tribble <ptribble@localhost.localdomain>
2014-06-02 11:04:08 -0700

    Fix

--- src/s3/SyncToS3.java
+++ src/s3/SyncToS3.java
@@ -40,11 +40,11 @@ public class SyncToS3 implements Runnable {
     }
 
     String convertObject(String what, String operation) {
-        
-        if (what.contains("C:\\")) {
-            what = what.replace("C:\\", "");
+
+        if (what.substring(1, 1).contains(":")) {
+            what = what.substring(2, what.length());
         }
-        
+
         if (what.contains("/")) {
             what = what.replace("/", File.separator);
         }
